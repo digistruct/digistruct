73345cc1b54392f9609014750e5523e6bad811ed
dr3wy98 <andrewdurnford732@gmail.com>
2019-10-17 14:09:25 +1100

    added navbar

--- react/src/components/app/app.js
+++ react/src/components/app/app.js
@@ -1,19 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Counter from '../counter/counter.js'
-import logo from '../../assets/images/logo.svg'
-import './app.css'
+import Home from '../home/home.js'
+import { Link, Route, BrowserRouter, Switch } from 'react-router-dom'
+
+import {
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown
+} from 'reactstrap'
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      <Counter />
+    <div>
+      <BrowserRouter>
+
+        <Navbar color="light" light expand="md">
+          <NavbarBrand tag={Link} to="/">digistruct</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink tag={Link} to="/">home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/counter">counter</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+              </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/counter" component={Counter} />
+        </Switch>
+
+      </BrowserRouter>
     </div>
   )
 }
